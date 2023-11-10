4461c3c63066c1903388ed597efa5d8c50d9d0c3
Guido-Panaccio <66895464+Guido-Panaccio@users.noreply.github.com>
2023-11-10 17:10:15 -0300

    Agrego scripts para database

---
+++ sistema-interno-hospitales/database/1-CREATE DATABASE hospitales.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE "hospitales"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Argentina.1252'
+    LC_CTYPE = 'Spanish_Argentina.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ sistema-interno-hospitales/database/2-CREATE TABLE Usuarios.sql
@@ -0,0 +1,15 @@
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'Usuarios') THEN
+        CREATE TABLE Usuarios (
+            idUsuario SERIAL PRIMARY KEY,
+            apellido VARCHAR(50) NOT NULL,
+			nombre VARCHAR(50) NOT NULL,
+            tipoDocumento VARCHAR(3) NOT NULL,
+			documento INT NOT NULL,
+			direccion VARCHAR(3),
+			telefono VARCHAR(50),
+            contraseña VARCHAR(255)
+        );
+    END IF;
+END $$;
