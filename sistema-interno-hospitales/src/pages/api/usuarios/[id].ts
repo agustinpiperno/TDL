b58407a39c3545a2664281cc0c7f4bce3ee69214
Guido-Panaccio <66895464+Guido-Panaccio@users.noreply.github.com>
2023-11-09 23:39:11 -0300

    Agrego POST y vista de id

--- sistema-interno-hospitales/src/pages/api/usuarios/[id].ts
+++ sistema-interno-hospitales/src/pages/api/usuarios/[id].ts
@@ -5,12 +5,14 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
     switch (method) {
         case 'GET':
-            return res.status(200).json('obteniendo usuario con id')
+            return res.status(200).json('obteniendo usuario con id: ' + req.query.id)
         case 'PUT':
-            return res.status(200).json('actualizando usuario con id')
+            return res.status(200).json('actualizando usuario con id: ' + req.query.id)
+        case 'POST':
+            return res.status(200).json('agregando usuario')
         case 'DELETE':
-            return res.status(200).json('eliminando usuario con id')
+            return res.status(200).json('eliminando usuario con id: ' + req.query.id)
         default:
-            return res.status(400).json("eliminando usuario con id")
+            return res.status(400).json('error')
     }
 }
