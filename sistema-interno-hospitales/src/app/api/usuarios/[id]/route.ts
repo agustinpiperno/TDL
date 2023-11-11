74f119591b3a43f1333767081efd74e77012ec4b
Agustin <agustin.piperno@gmail.com>
2023-11-11 01:43:03 -0300

    Correccion de error de tipado al definir los parametros

--- sistema-interno-hospitales/src/app/api/usuarios/[id]/route.ts
+++ sistema-interno-hospitales/src/app/api/usuarios/[id]/route.ts
@@ -1,23 +1,25 @@
 import {NextRequest, NextResponse} from "next/server"
 
 interface Params {
-    id: string
+    params: {
+        id: string
+    }
 }
 
-export const GET = async (req: NextRequest, { params } : { params: Params }) => {
+export const GET = async (req: NextRequest, { params } : Params) => {
     return NextResponse.json(
         {message: `obteniendo usuario con id ${params.id}`}, 
         {status: 200})
 }
 
-export const PUT = async (req: NextRequest, { params }: { params: { id: string } }) => {
+export const PUT = async (req: NextRequest,  { params } : Params) => {
     return NextResponse.json(
         {message: `actualizando usuario con id ${params.id}`}, 
         {status: 200}
     )
 }
 
-export const DELETE = async (req: NextRequest, { params }: { params: { id: string } }) => {
+export const DELETE = async (req: NextRequest,  { params } : Params) => {
     return NextResponse.json(
         {message: `eliminando usuario con id ${params.id}`}, 
         {status: 200}
