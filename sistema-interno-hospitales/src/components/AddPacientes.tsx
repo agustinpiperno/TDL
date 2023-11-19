57040d285584a3d6ea5b6ef4e028a1eb4c7b9ff6
Guido-Panaccio <66895464+Guido-Panaccio@users.noreply.github.com>
2023-11-19 19:51:12 -0300

    Creación templete Modal para Pacientes

--- sistema-interno-hospitales/src/components/AddPacientes.tsx
+++ sistema-interno-hospitales/src/components/AddPacientes.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
-import {AiOutlinePlus} from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
+import Modal from "./Modal";
 
 const AddPaciente = () => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
     return (
         <div>
-            <Button className="w-full mt-6 " name="btnAddPacientes" type="submit">
-                Agregar Paciente 
-                <AiOutlinePlus className="ml-2" size={18}/>
-            </Button>        
+            <Button className="w-full mt-6 " name="btnAddPacientes" type="submit"
+                onClick={() => setModalOpen(true)}>
+                Agregar Paciente
+                <AiOutlinePlus className="ml-2" size={18} />
+            </Button>
+
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+                modal for add paciente
+            </Modal>
+
         </div>
     );
 };
