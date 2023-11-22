7b2491519d921fe289f1be4119e748317e1092f9
Agustin <agustin.piperno@gmail.com>
2023-11-21 22:54:22 -0300

    Se agregaron JWT para manejo de cookies y ruteo luego de autenticacion de login

--- sistema-interno-hospitales/src/app/mainPage/layout.tsx
+++ sistema-interno-hospitales/src/app/mainPage/layout.tsx
@@ -1,11 +1,32 @@
+"use client"
+
 import NavbarMainPage from "@/components/NavbarMainPage"; // Adjust the path based on your project structure
 import "@/styles/globals.css"; // Import your global styles here
+import { useEffect } from "react";
+import axios, {AxiosError} from "axios";
+import {useRouter} from "next/navigation"
+
+interface UserResponse{
+    usuario: string | null;
+    error: AxiosError | null
+}
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+const router = useRouter()
+
+useEffect(() => {
+    (async () => {
+        const {usuario, error} = await getUser()
+        if(error){
+            router.push("/home")
+            return
+        }
+    })
+}, [router])
 	return (
 		 <div className="h-screen flex flex-col justify-center items-center bg-blue-950">
             <NavbarMainPage />
@@ -14,4 +35,22 @@ export default function RootLayout({
             </div>
         </div>
 	);
+}
+
+async function getUser(): Promise<UserResponse>{
+    console.log("aaaaa")
+    try{
+        const {data} = await axios.get("/api/auth")
+        return {
+            usuario: data,
+            error: null,
+        }
+    }
+    catch(e){
+        const error = e as AxiosError
+        return {
+            usuario: null,
+            error,
+        }
+    }
 }
--- sistema-interno-hospitales/src/components/WelcomeBanner.tsx
+++ sistema-interno-hospitales/src/components/WelcomeBanner.tsx
@@ -1,12 +1,25 @@
+import { cookies } from "next/headers"
+import jwt, {JwtPayload} from 'jsonwebtoken'
+
 const WelcomeBanner = () => {
-    
-    const nombreUsuario = "Usuario"
+    const cookieStore = cookies()
+    const token = cookieStore.get("OutSiteJWT")
+
+    let nombreUsuario = "Usuario";
+    if (token) {
+        const decodedToken = jwt.verify(token.value, 'secret') as JwtPayload
+        const firstKey = Object.keys(decodedToken)[0]
+        nombreUsuario = decodedToken[firstKey]
+
+    }
+
     return (
         <div className="py-2 border-s-zinc-200 w-full z-10 top-0">
             <div className="container flex items-center justify-between">
-                Bienvenido {nombreUsuario}
+                {`Bienvenido, ${nombreUsuario}`}
             </div>
         </div>
     );
 };
+
 export default WelcomeBanner;
