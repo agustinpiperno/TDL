2f85adc77bacb9a08cc2ca8c0bab6c1d009533d0
fdelpup <fdelpup@fi.uba.ar>
2023-12-03 23:35:12 -0300

    arregle el div que tiraba error en administrar cuenta

--- sistema-interno-hospitales/src/app/pacientes/layout.tsx
+++ sistema-interno-hospitales/src/app/pacientes/layout.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
 import NavbarMainPage from "@/components/NavbarMainPage"; // Adjust the path based on your project structure
 import "@/styles/globals.css"; // Import your global styles here
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import axios, {AxiosError} from "axios";
-import {useRouter} from "next/navigation"
 
-interface UserResponse{
-    usuario: string | null;
-    error: AxiosError | null
+interface UserResponse {
+	usuario: string | null;
+	error: AxiosError | null;
 }
 
 export default function RootLayout({
@@ -16,48 +16,44 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
+	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
 
-const router = useRouter()
-const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		const fetchUser = async () => {
+			const { usuario, error } = await getUser();
+			if (error) {
+				router.push("/home");
+			}
+			setIsLoading(false);
+		};
+		fetchUser();
+	}, [router]);
 
-useEffect(() => {
-    const fetchUser = (async () => {
-        const {usuario, error} = await getUser()
-        if(error){
-            router.push("/home")
-        }
-        setIsLoading(false);
-    })
-    fetchUser()
-}, [router])
-
-if (isLoading) {
-    return null; // Para que no renderize la pagina hasta que termine getUser
-}
+	if (isLoading) {
+		return null; // Para que no renderize la pagina hasta que termine getUser
+	}
 
 	return (
-		 <div className="h-screen flex flex-col justify-center items-center bg-blue-950">
-            <NavbarMainPage />
-            <div className="flex-1 flex justify-center items-center">
-                <div className="bg-white p-20 rounded-md shadow-2xl">{children}</div>
-            </div>
-        </div>
+		<div className="h-screen flex flex-col justify-center items-center bg-blue-950">
+			<NavbarMainPage />
+			<div className="bg-white p-10 rounded-md shadow-2xl">{children}</div>
+		</div>
 	);
 }
 
-async function getUser(): Promise<UserResponse>{
-    try{
-        const {data} = await axios.get("/api/auth")
-        return {
-            usuario: data,
-            error: null,
-        }
-    }
-    catch(e){
-        const error = e as AxiosError
-        return {
-            usuario: null,
-            error,
-        }
-    }
+async function getUser(): Promise<UserResponse> {
+	try {
+		const { data } = await axios.get("/api/auth");
+		return {
+			usuario: data,
+			error: null,
+		};
+	} catch (e) {
+		const error = e as AxiosError;
+		return {
+			usuario: null,
+			error,
+		};
+	}
 }
