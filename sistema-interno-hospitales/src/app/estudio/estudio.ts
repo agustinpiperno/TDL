725fce67f2bea0a31515e173cdef375028d5f282
Guido-Panaccio <66895464+Guido-Panaccio@users.noreply.github.com>
2023-12-17 19:56:50 -0300

    Elimino codigo comentado en estudios

--- sistema-interno-hospitales/src/app/estudio/estudio.ts
+++ sistema-interno-hospitales/src/app/estudio/estudio.ts
@@ -3,37 +3,6 @@ import {IExamen} from "../../types/examen";
 
 const apiUrl = `http://localhost:3000/api/estudio`;
 
-// export const insertarEstudio = async (infoEstudio: IEstudio) : Promise<IEstudio | void> => {
-    
-//     const estudioNuevo = {
-//         estudio: {
-//             tipoEstudio: infoEstudio.tipoEstudio,
-//             resultado: infoEstudio.resultado,
-//             examenesIdExamen: infoEstudio.examenesIdExamen,
-//             Estudio: infoEstudio.Estudio
-//         }
-//     };
-
-//     try {
-//         const response = await fetch(apiUrl, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(estudioNuevo)
-//         });
-
-//         if(response.ok) {
-//             const data = await response.json();
-//             return data.pacientes
-//         }else {
-//             throw new Error('Error al querer ingresar al paciente');
-//         } 
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// };
-
 export const insertarEstudio = async (infoEstudio: IEstudio): Promise<IEstudio | void> => {
     const formData = new FormData();
     formData.append('tipoEstudio', infoEstudio.tipoEstudio);
@@ -84,35 +53,7 @@ export const editarEstudio = async (infoEstudio: IEstudio) : Promise<IEstudio | 
         }
     } catch (error) {
         console.error('Error:', error);
-    }
-
-    // const estudioEditar = {
-    //     estudio:{
-    //         idEstudio: infoEstudio.idEstudio,
-    //         tipoEstudio: infoEstudio.tipoEstudio,
-    //         resultado: infoEstudio.resultado,
-    //         Examen: infoEstudio.Examen
-    //     }
-    // };
-
-    // try {
-    //     const response = await fetch(apiUrl, {
-    //         method: 'PUT',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(estudioEditar)
-    //     });
-    
-    //     if (response.ok) {
-    //         const data = await response.json();
-    //         return data
-    //     }else {
-    //         throw new Error('Error al querer editar el estudio');
-    //     } 
-    // } catch (error) {
-    //     console.error('Error:', error);
-    // }   
+    } 
 };
 
 export const eliminarEstudio = async (idEstudio: number) : Promise<void> => {
